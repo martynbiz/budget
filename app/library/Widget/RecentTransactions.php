d07608c456be2574c56ed73dd9e65c06fc3bbcce
Martyn Bissett <martynbissett@yahoo.co.uk>
2017-08-01 19:04:18 +0100

    Set fund id for recent trans widget

--- app/library/Widget/RecentTransactions.php
+++ app/library/Widget/RecentTransactions.php
@@ -16,8 +16,10 @@ class RecentTransactions extends Base
 
         $userId = $container->get('auth')->getAttributes()['id'];
         $currentUser = $container->get('model.user')->find($userId);
+        $fundId = $container->get('session')->get(SESSION_FILTER_FUND);
 
         $transactions = $currentUser->transactions()
+            ->where('fund_id', $fundId)
             ->orderBy('purchased_at', 'desc')
             ->orderBy('created_at', 'desc')
             ->limit(5)
