816abdeb88cb9c0bac749e39aab9fb952d8470cb
Martyn Bissett <martynbissett@yahoo.co.uk>
2017-09-23 00:26:58 +0100

    Add bearer methods to utils

--- app/library/Controller/Api/ApiController.php
+++ app/library/Controller/Api/ApiController.php
@@ -12,6 +12,7 @@ use Slim\Container;
 
 use App\Controller\BaseController;
 use App\Model\Transaction;
+use App\Utils;
 
 class ApiController extends BaseController
 {
@@ -26,7 +27,7 @@ class ApiController extends BaseController
 
         // if token is present, we'll set that here so that it is available to
         // all getCurrentUser method calls in API
-        $this->apiToken = $this->getBearerToken();
+        $this->apiToken = Utils::getBearerToken();
     }
 
     /**
@@ -47,43 +48,5 @@ class ApiController extends BaseController
     	}
 
     	return $this->currentUser;
-    }
-
-    /**
-     * get access token from header
-     * */
-    function getBearerToken()
-    {
-        $headers = $this->getAuthorizationHeader();
-        // HEADER: Get the access token from the header
-        if (!empty($headers)) {
-            if (preg_match('/Bearer\s(\S+)/', $headers, $matches)) {
-                return $matches[1];
-            }
-        }
-        return null;
-    }
-
-    /**
-     * Get hearder Authorization
-     * */
-    function getAuthorizationHeader()
-    {
-        $headers = null;
-        if (isset($_SERVER['Authorization'])) {
-            $headers = trim($_SERVER["Authorization"]);
-        }
-        else if (isset($_SERVER['HTTP_AUTHORIZATION'])) { //Nginx or fast CGI
-            $headers = trim($_SERVER["HTTP_AUTHORIZATION"]);
-        } elseif (function_exists('apache_request_headers')) {
-            $requestHeaders = apache_request_headers();
-            // Server-side fix for bug in old Android versions (a nice side-effect of this fix means we don't care about capitalization for Authorization)
-            $requestHeaders = array_combine(array_map('ucwords', array_keys($requestHeaders)), array_values($requestHeaders));
-            //print_r($requestHeaders);
-            if (isset($requestHeaders['Authorization'])) {
-                $headers = trim($requestHeaders['Authorization']);
-            }
-        }
-        return $headers;
     }
 }
